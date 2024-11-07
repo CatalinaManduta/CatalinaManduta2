fed1d409033cb32216c7d4a72bc829bcc4cc53f4
CatalinaManduta <catalina.manduta@yahoo.com>
2024-11-07 09:21:04 -0500

    Settings

--- Profile/settings.py
+++ Profile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y)6$%un_1vq)8y$jkv2du_6cby1#ctf_s(r8cok-1o20#-j9tq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['CatalinaM.eu.pythonanywhere.com']
 
 
 # Application definition
@@ -116,14 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = '/home/CatalinaM/CatalinaManduta/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
